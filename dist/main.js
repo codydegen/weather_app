94288529eff61f03d4e5f22e10c3d184030014fb
codydegen <53412558+codydegen@users.noreply.github.com>
2021-04-08 11:26:15 -0400

    fetch weather from city

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,101 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/config.js":
+/*!***********************!*\
+  !*** ./src/config.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const config = {
+  OPENWEATHERMAP_API_TOKEN: 'd2227e66123b92ad3505d44f24fddf31',
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (config);
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log('hello world');
-const bbb = 5;
-const ccc = 3;
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _config_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./config.js */ "./src/config.js");
+/* eslint-disable no-console */
 
-console.log(bbb + ccc);
+
+async function getWeather(location) {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Columbus&appid=${_config_js__WEBPACK_IMPORTED_MODULE_0__.default.OPENWEATHERMAP_API_TOKEN}`);
+  const data = await response.json();
+  console.log(data);
+}
+getWeather('columbus');
+
+})();
 
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-console.log('hello world');
-const bbb = 5;
-const ccc = 3;
+/* eslint-disable no-console */
+import config from "./config.js";
 
-console.log(bbb + ccc);
+async function getWeather(location) {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Columbus&appid=${config.OPENWEATHERMAP_API_TOKEN}`);
+  const data = await response.json();
+  console.log(data);
+}
+getWeather('columbus');
