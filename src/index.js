b6cc443376bf00f8f88ea6164900a0b8b9d29833
codydegen <53412558+codydegen@users.noreply.github.com>
2021-04-08 10:34:46 -0400

    finish project setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('hello world');
+const bbb = 5;
+const ccc = 3;
+
+console.log(bbb + ccc);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    sourceMapFilename: '[name].js.map',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  mode: 'development',
+  devtool: 'source-map',
+};
